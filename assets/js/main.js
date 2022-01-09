7aa292adbe97e8dd09d2c79398ad9f82a8357d22
Satin Best <2ksky92@gmail.com>
2022-01-09 12:29:26 +0330

    start develope wordpress theme

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+// slider in main page
+$('#main_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplayHoverPause:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+// article slider
+$('#article_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplayHoverPause:true,
+    autoplay:true,
+    lazyLoad:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+// course slier
+$('#course-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplayHoverPause:true,
+    autoplay:true,
+    lazyLoad:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+// related post in single page slider
+$('#relate_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    autoplayHoverPause:true,
+    autoplay:true,
+    lazyLoad:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
