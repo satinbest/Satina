d87b26f5063dca72c84f2bf042d556f06c884acd
Satin Best <2ksky92@gmail.com>
2022-01-14 14:40:21 +0330

    dynamic tv single page

--- single-tv.php
+++ single-tv.php
@@ -1,1 +1,103 @@
-<?php
+<?php get_header(); ?>
+
+    <!--start main single page-->
+
+    <div class="single-page">
+        <div class="container">
+            <div class="main-single">
+                <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <article class="post-single">
+                            <header>
+                                <h1><?php the_title(); ?></h1>
+                                <div class="post-meta">
+                                    <i class="fas fa-clock"></i>
+                                    <span><?php the_time('j F Y'); ?></span>
+                                </div>
+                                <div class="post-meta">
+                                    <i class="fas fa-user"></i>
+                                    <span><?php the_author(); ?></span>
+                                </div>
+                                <div class="post-meta">
+                                    <i class="fas fa-folder-open"></i>
+                                    <span><?php the_category(' , '); ?></span>
+                                </div>
+                                <div class="post-meta">
+                                    <i class="fas fa-eye"></i>
+                                    <span><?php if(function_exists('the_views')) { the_views(); } ?></span>
+                                </div>
+                            </header>
+                            <div class="post-thumbnail">
+                                <figure>
+                                    <?php
+                                    $video = get_post_meta(get_the_ID(),'satina_video_tv',true);
+                                    $poster = get_the_post_thumbnail_url();
+                                    if (!empty($video)){
+                                        $attr = array(
+                                                'src' => $video,
+                                                'width' => '1200',
+                                                'height' => '750',
+                                                'poster' => $poster
+                                        );
+                                        echo wp_video_shortcode($attr);
+                                    }else{
+                                        the_post_thumbnail();
+                                    }
+                                    ?>
+                                </figure>
+                            </div>
+                            <div class="content-single">
+                                <?php the_content(); ?>
+                            </div>
+                        </article>
+                    <?php
+                    endwhile;
+                endif;
+                ?>
+                <section class="related-post">
+                    <div class="related-head">
+                        <h4>مطالب زیر را حتما مطالعه نمایید</h4>
+                    </div>
+
+                    <div class="article-slider">
+                        <div class="owl-carousel owl-theme" id="relate_slider">
+                            <?php
+                            $related = get_posts( array(
+                                'category__in' => wp_get_post_categories($post->ID),
+                                'numberposts' => 5,
+                                'post__not_in' => array($post->ID)
+                            ) );
+                            if( $related ) foreach( $related as $post ) {
+                                setup_postdata($post); ?>
+                                <div class="item box-article">
+                                    <?php the_post_thumbnail(); ?>
+                                    <h2><a href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a></h2>
+                                    <?php the_excerpt(); ?>
+                                    <div class="btn-more">
+                                        <a href="<?php the_permalink(); ?>">بیشتر بخوانید</a>
+                                    </div>
+                                </div>
+                            <?php }
+                            wp_reset_postdata(); ?>
+                        </div>
+                    </div>
+                </section>
+                <div class="comment-box">
+                    <div class="related-head">
+                        <h4>نظرات : </h4>
+                    </div>
+                    <?php
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
+                    ?>
+                </div>
+
+            </div>
+
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
