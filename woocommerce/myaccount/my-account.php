a4b3c1940411e917e7f34a0a9d4e544a89d31d11
Satin Best <2ksky92@gmail.com>
2022-01-15 22:21:31 +0330

    style dashboard woocammerce

--- woocommerce/myaccount/my-account.php
+++ woocommerce/myaccount/my-account.php
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) || exit;
  */
 do_action( 'woocommerce_account_navigation' ); ?>
 
-<div class="woocommerce-MyAccount-content">
+<div class="woocommerce-MyAccount-content satina-content-account">
 	<?php
 		/**
 		 * My Account content.
