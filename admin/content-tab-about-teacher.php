53f1fcfb9863355224f4a81958b64a42b9c02d54
Satin Best <2ksky92@gmail.com>
2022-01-15 14:49:28 +0330

    dynamic list tab  course

---
+++ admin/content-tab-about-teacher.php
@@ -0,0 +1,41 @@
+<?php
+//Add tab teacher
+add_filter( 'woocommerce_product_tabs', 'woocommerce_product_teacher' );
+function woocommerce_product_teacher( $tabs ) {
+
+    $tabs['course_teacher'] = array(
+        'title' 	=> __( 'مدرس', 'woocommerce' ),
+        'priority' 	=> 10,
+        'callback' 	=> 'woocommerce_product_teacher_content'
+    );
+
+    return $tabs;
+
+}
+function woocommerce_product_teacher_content() {
+    $teacher_name = get_post_meta(get_the_ID(),'satina_course_teacher_name',true);
+    if (!empty($teacher_name)){
+        ?>
+        <div class="course-teacher">
+            <?php
+            $teacher_pic = get_post_meta(get_the_ID(), 'satina_course_teacher_pic', true);
+            if (!empty($teacher_pic)){
+                ?>
+                <div class="teacher-pic">
+                    <img src="<?php echo $teacher_pic; ?>" alt="Teacher Pic">
+                </div>
+                <?php
+            }
+            ?>
+            <div class="teacher-about">
+                <h5><?php echo $teacher_name; ?></h5>
+                <?php $teacher_about = get_post_meta(get_the_ID(),'satina_course_teacher_about',true);
+                if (!empty($teacher_about)){
+                    echo $teacher_about;
+                }
+                ?>
+            </div>
+        </div>
+        <?php
+    }
+}
---
+++ admin/content-tab-lesson.php
@@ -0,0 +1,59 @@
+<?php
+//add tab course list
+add_filter( 'woocommerce_product_tabs', 'woocommerce_product_course_list' );
+function woocommerce_product_course_list( $tabs ) {
+
+    $tabs['lesson_list'] = array(
+        'title' 	=> __( 'فهرست جلسات', 'woocommerce' ),
+        'priority' 	=> 20,
+        'callback' 	=> 'woocommerce_product_lesson_list_content'
+    );
+
+    return $tabs;
+}
+function woocommerce_product_lesson_list_content(){
+    ?>
+<?php
+$course = get_post_meta(get_the_ID(),'satina_group_lesson',true);
+    if (!empty($course)){ ?>
+    <div class="lesson-course">
+        <ul>
+            <?php foreach ($course as $item){ ?>
+            <li>
+                <h4><?php echo $item['satina_course_chapter_title']; ?>
+                    <i class="fas fa-angle-down"></i>
+                </h4>
+                <ul>
+                    <div class="meta-course">
+                        <div class="time-course">
+                            <i class="fas fa-clock"></i>
+                            <span>مدت زمان فصل :</span>
+                            <span><?php echo $item['satina_course_chapter_time']; ?></span>
+                        </div>
+                        <div class="dl-course">
+                            <a href="<?php echo $item['satina_course_chapter_link']; ?>" target="_blank">
+                                <i class="fas fa-download"></i>
+                                لینک دانلود فصل
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                    <?php foreach ($item['satina_course_chapter_lesson'] as $value) { ?>
+                    <li>
+                        <i class="fas fa-check"></i>
+                        <?php echo $value;?>
+                    </li>
+                    <?php } ?>
+                </ul>
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
+    <?php
+    }else{
+        echo "سرفصلی برای این محصول تعریف نشده است.";
+    }
+    ?>
+<?php
+}
+?>
